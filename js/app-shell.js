0379e49ff050d7faa46efcdfe9c9539d1fbac422
ACasinha <andrecasinha@gmail.com>
2026-09-11 12:22:52 +0100

    Fix indentation and formatting in app-shell.js

--- js/app-shell.js
+++ js/app-shell.js
@@ -75,7 +75,8 @@ function _activarShell(perfil) {
         }
         if (navigator.onLine && typeof syncSincronizarEstadosEmRevisao === 'function') {
           syncSincronizarEstadosEmRevisao();
-      });
+        }
+      })
       .catch(function() {});
   }
 
